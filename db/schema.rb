a30056805cd1dfef27e6975c276bbd54bf13bc45
Your Name <you@example.com>
2019-09-04 14:33:51 -0400

    remove sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_08_07_153315) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "user_words", force: :cascade do |t|
     t.integer "user_id"
     t.integer "word_id"
